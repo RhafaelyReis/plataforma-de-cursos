cee5d63b6ec9f0b59200af8e2d8288f36265877e
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2026-05-19 13:16:44 -0400

    feat: implementa cadastro de usuários com persistência no H2

--- plataforma-cursos/src/main/java/br/edu/ifms/plataforma_cursos/controller/UserController.java
+++ plataforma-cursos/src/main/java/br/edu/ifms/plataforma_cursos/controller/UserController.java
@@ -1,5 +1,28 @@
 package br.edu.ifms.plataforma_cursos.controller;
 
+import br.edu.ifms.plataforma_cursos.model.User;
+import br.edu.ifms.plataforma_cursos.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+
+        userRepository.save(user);
+
+        return "redirect:/";
+    }
 }
--- plataforma-cursos/src/main/java/br/edu/ifms/plataforma_cursos/repository/UserRepository.java
+++ plataforma-cursos/src/main/java/br/edu/ifms/plataforma_cursos/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package br.edu.ifms.plataforma_cursos.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.edu.ifms.plataforma_cursos.model.User;
 
-}
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+    
+} 
